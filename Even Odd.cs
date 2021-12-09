23b76b4406e70baf072991e33afaed9219ef7337
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-09 16:07:37 +0530

    Add files via upload

---
+++ Even Odd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Excercise4
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("It is Even number");
+        }
+        else
+        {
+            Console.WriteLine("It is odd number");
+        }
+    }
+    
+}
+
