a29ccf240809129c8c7d19e435b4228930faa32b
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-09 16:06:08 +0530

    Add files via upload

---
+++ Swap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Excercise3
+{
+     static void Main(string[] args)
+    {
+        int x, y, temp;
+        Console.Write("\ninput the First number : ");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("\ninput the Second number : ");
+        y = int.Parse(Console.ReadLine());
+        temp = x;
+        x = y;
+        y = temp;
+        Console.Write("After swap :");
+        Console.Write("\ninput the First number : "+x);
+        Console.Write("\ninput the Second number : "+y);
+        Console.Read();
+
+    }
+}
