5d031792aea9a2e2a998e83aede2ce9c36b04dff
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-09 23:48:41 +0530

    Add files via upload

---
+++ Algo.Excercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Excercise1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test(1, 2));
+        Console.WriteLine(test(2, 3));
+        Console.WriteLine(test(2, 2));
+        Console.ReadLine();
+    }
+    static int test(int x, int y)
+    {
+        return x == y ? (x + y) * 3 : x + y;
+    }
+}
+
