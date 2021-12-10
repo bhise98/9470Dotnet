ddb5948165a320a7539ddb6a78f340a4409368e4
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-10 23:15:49 +0530

    Add files via upload

---
+++ If else.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ifelse
+{
+    class ifelse
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test("if else"));
+            Console.WriteLine(test("else"));
+            Console.ReadLine();
+        }
+        static string test(string s)
+        {
+            if(s.Length > 2 && s.Substring(0, 2).Equals("if"))
+            {
+                return s;
+            }
+            return "if" + s;
+        }
+    }
+}
+    
