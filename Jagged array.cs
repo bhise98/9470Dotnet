bd23fcf1a99d6c31fbf016126ae024ea43112c8f
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-17 11:47:43 +0530

    Add files via upload

---
+++ Jagged array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Studytonight
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+ int[][] arr = new int[][]
+ {
+ new int[] { 0, 4 },
+ new int[] { 40, 44 },
+ new int[] { 400, 401, 410, 411 }
+ };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write(arr[i][j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
