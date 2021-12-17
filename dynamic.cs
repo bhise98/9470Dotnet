af2f771ac9c0212e89863e53ed67f2ff3b19a0ff
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-17 12:19:29 +0530

    Add files via upload

---
+++ dynamic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class Demo
+    {
+        dynamic data = 14;
+        public int method(int x, int y)
+        {
+            return (x + y) * data;
+        }
+
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Demo obj = new Demo();
+            dynamic value1 = 5;
+            dynamic value2 = 6;
+        dynamic str = "your result is:";
+            Console.WriteLine(str + obj.method(value1, value2));
+            Console.ReadLine();
+        }
+    }
+
+
+
