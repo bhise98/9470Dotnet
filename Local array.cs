11abd4f97f8678a094b063b1714a52be08035887
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-17 11:48:34 +0530

    Add files via upload

---
+++ Local array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+    namespace Local.a
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //float
+            var a = 3.755;
+            //string
+            var s = "javascript";
+            //array
+            var arr = new [] {1,2,3};
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[1]);
+        }
+    }
+        
+        
+}
+
+
+
