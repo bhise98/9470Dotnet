47759deaf77df74db0e60315bdb626a2645dc80f
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-18 13:33:25 +0530

    Add files via upload

---
+++ Single D. aaray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Single.D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 50, 51, 500, 501, 510 };
+            int len = arr.Length;
+            Console.WriteLine("Length of Array " + len);
+            Console.WriteLine(arr[0] + " " + arr[4]);
+        }
+    }
+}
