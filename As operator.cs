b7c0db07b91d0b7ce3e68d17bc3b77a90a8ad064
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-20 11:47:13 +0530

    Add files via upload

---
+++ As operator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Demo
+{
+    static void Main()
+    {
+        object[] obj = new object[2];
+        obj[0] = "Pratiksha";
+        obj[1] = 32;
+        for (int i = 0; i < obj.Length; ++i)
+        {
+            string s = obj[i] as string;
+            Console.Write("{0}: ", i);
+            if (s != null)
+          Console.WriteLine("'" + s + "'");
+           else
+                Console.WriteLine("This is not a string!");
+        }
+        Console.ReadKey();
+    }
+}
