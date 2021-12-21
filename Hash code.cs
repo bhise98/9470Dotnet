d82afca4f1dacc15d9cc017c13b11daabefded52
bhise98 <95096461+bhise98@users.noreply.github.com>
2021-12-21 10:07:22 +0530

    Add files via upload

---
+++ Hash code.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+namespace object_Class_Gethashcode
+{
+    class Program
+    {
+        class HashC
+        {
+            string name;
+ public HashC(string name)
+            {
+                this.name = name;
+            }
+            public override int GetHashCode()
+            {
+                string n = ToString();
+                return n.GetHashCode();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            HashC ob1 = new HashC("Pratiksha Dhanraj Bhise");
+            Console.WriteLine(ob1.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+}
